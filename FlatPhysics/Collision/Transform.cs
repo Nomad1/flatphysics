182b45fcc570a2e96f7f2d92c091f38a516396a1
Alexei Garbuzenko <gorbuz@gmail.com>
2019-12-02 23:41:49 +0200

    Update Transform.cs

--- FlatPhysics/Collision/Transform.cs
+++ FlatPhysics/Collision/Transform.cs
@@ -6,9 +6,12 @@ using System.Numerics;
 
 namespace FlatPhysics.Collision
 {
+    /// <summary>
+    /// Helper immutable struct containing offset point and rotation angle among with it's sine and cosine values
+    /// </summary>
     public struct Transform
     {
-        public readonly static Transform Empty = default(Transform);// new Transform(Vector2.Zero, 0);
+        public readonly static Transform Empty = default(Transform);
 
         private readonly Vector2 m_offset;
 
@@ -33,9 +36,14 @@ namespace FlatPhysics.Collision
 
         public bool IsValid
         {
-            get { return m_cos != 0.0f || m_sin != 0.0f; }
+            get { return m_cos != 0.0f && m_sin != 0.0f; } // there is no way sin and cos could be zero
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:FlatPhysics.Collision.Transform"/> struct
+        /// </summary>
+        /// <param name="offset">Offset.</param>
+        /// <param name="angle">Angle.</param>
         public Transform(Vector2 offset, float angle)
         {
             m_offset = offset;
@@ -48,12 +56,63 @@ namespace FlatPhysics.Collision
 #endif
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:FlatPhysics.Collision.Transform"/> struct
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="angle">Angle.</param>
         public Transform(float x, float y, float angle)
             : this(new Vector2(x, y), angle)
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:FlatPhysics.Collision.Transform"/> struct using new shift point and angle from other transform
+        /// </summary>
+        /// <param name="offset">Offset.</param>
+        /// <param name="otherTransform">Other transform.</param>
+        public Transform(Vector2 offset, Transform otherTransform)
+        {
+            m_offset = offset;
+            m_angle = otherTransform.m_angle;
+            m_sin = otherTransform.m_sin;
+            m_cos = otherTransform.m_cos;
+        }
+
+
+        /// <summary>
+        /// Rotate and shift the point
+        /// </summary>
+        /// <returns>The transformed point</returns>
+        /// <param name="v">Source point</param>
+        public Vector2 Mul(Vector2 v)
+        {
+            float x = (m_cos * v.X - m_sin * v.Y);
+            float y = (m_sin * v.X + m_cos * v.Y);
+
+            return new Vector2(x, y) + m_offset;
+        }
+
+        /// <summary>
+        /// Rotate the point without shifting
+        /// </summary>
+        /// <returns>The transformed point</returns>
+        /// <param name="v">Source point</param>
+        public Vector2 MulR(Vector2 v)
+        {
+            float x = (m_cos * v.X - m_sin * v.Y);
+            float y = (m_sin * v.X + m_cos * v.Y);
+
+            return new Vector2(x, y);
+        }
+
+        /// <summary>
+        /// Transposed operation to Mul
+        /// </summary>
+        /// <returns>The transformed point</returns>
+        /// <param name="v">Source point</param>
         public Vector2 MulT(Vector2 v)
         {
             float px = v.X - m_offset.X;
@@ -64,6 +123,11 @@ namespace FlatPhysics.Collision
             return new Vector2(x, y);
         }
 
+        /// <summary>
+        /// Transposed operation to MulR
+        /// </summary>
+        /// <returns>The transformed point</returns>
+        /// <param name="v">Source point</param>
         public Vector2 MulTR(Vector2 v)
         {
             float x = (m_cos * v.X + m_sin * v.Y);
@@ -72,22 +136,8 @@ namespace FlatPhysics.Collision
             return new Vector2(x, y);
         }
 
-        public Vector2 MulR(Vector2 v)
-        {
-            float x = (m_cos * v.X - m_sin * v.Y);
-            float y = (m_sin * v.X + m_cos * v.Y);
-
-            return new Vector2(x, y);
-        }
-
-        public Vector2 Mul(Vector2 v)
-        {
-            float x = (m_cos * v.X - m_sin * v.Y);
-            float y = (m_sin * v.X + m_cos * v.Y);
-
-            return new Vector2(x, y) + m_offset;
-        }
-
+        // static helper methods
+       
         public static Vector2 Mul(Transform t, Vector2 v)
         {
             return t.Mul(v);
